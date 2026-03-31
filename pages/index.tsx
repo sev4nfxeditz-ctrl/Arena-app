9102f9b26bae14ae7285f46d99dfa0c886c9746c
sev4nfxeditz-ctrl <sev4nfxeditz@gmail.com>
2026-03-31 16:41:42 +0530

    Create homepage component

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Arena App</h1>
+            <p>This is the homepage of your application.</p>
+        </div>
+    );
+};
+
+export default HomePage;
